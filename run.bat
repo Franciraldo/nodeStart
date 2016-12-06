58e670bf867eb6403dd79439980e4d58d4e7cc31
Franciraldo <franciraldo@gmail.com>
2016-12-06 16:22:28 -0200

    Delete run.bat

--- run.bat
+++
@@ -1,1 +0,0 @@
-java -classpath .\bin;./lib\jetty-all.jar;./lib\javaee-api-7.0.jar;./lib\servlet-api.jar;./lib\fts-utils.jar;./lib\javatools.jar;./lib\log4j-1.2.9.jar ionicStart.run "E:/Desenv/Meus documentos/ionic/assessmentApp/www/" shell title:Assessment-App start:index.html %1 %2 %3 %4 %5
